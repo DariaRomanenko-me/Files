60b3e0e4a169789d56e338f5f665b15006d25045
DariaRomanenko-me <57621524+DariaRomanenko-me@users.noreply.github.com>
2020-01-16 20:33:13 +0300

    Update TaskF2.java

--- TaskF2.java
+++ TaskF2.java
@@ -4,7 +4,6 @@ package com.company;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class TaskF2 {
     public static void main(String[] args) throws IOException {
